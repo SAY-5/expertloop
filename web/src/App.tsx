052cb2c5f37e764d2c3e59b160e39654b748ea82
SAY-5 <say.apm35@gmail.com>
2026-09-08 16:11:22 -0700

    feat: mount compile section

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,17 @@
+import { CompileSection } from "./components/CompileSection";
 import { Hero } from "./components/Hero";
 import { WorldProvider } from "./store";
 import "./styles/hero.css";
+import "./styles/compile.css";
 
 export default function App() {
   return (
     <WorldProvider>
       <div id="top" />
       <Hero />
-      <main />
+      <main>
+        <CompileSection />
+      </main>
     </WorldProvider>
   );
 }
